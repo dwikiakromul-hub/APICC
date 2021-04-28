422d0a550b9132cea2cc04ba60b2969e99206f58
Dwiki akromul hudha <57337033+dwikiakromul-hub@users.noreply.github.com>
2021-04-28 11:56:35 +0700

    + Secure API

--- koneksi.php
+++ koneksi.php
@@ -8,6 +8,36 @@
                              "Database"=>"Dispatcher",
                              "CharacterSet"=>"UTF-8");
     $conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+    $user="BPBDCC";
+    $pass="salamTangguh!!";
+
+    function getToken() {
+        $header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
+    
+    // Create token payload as a JSON string
+        $payload = json_encode(['username' => $user, 'password' => $pass]);
+    
+    // Encode Header to Base64Url String
+        $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+    
+    // Encode Payload to Base64Url String
+        $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
+    
+    // Create Signature Hash
+        $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, 'S4L4mT499uH', true);
+    
+    // Encode Signature to Base64Url String
+        $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+    
+    // Create JWT
+        $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+    
+        return $jwt;
+    }
+
+    
+    
 ?>
 
 
